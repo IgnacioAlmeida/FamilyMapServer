08f5bb3ea2ff4f16c393b332e1418b6ba5c81c21
Ignacio R. de Almeida <57123713+IgnacioAlmeida@users.noreply.github.com>
2021-07-11 18:52:57 -0600

    Stubs and Java Docs created for DAO

--- src/main/java/DataAccess/PersonDAO.java
+++ src/main/java/DataAccess/PersonDAO.java
@@ -1,4 +1,52 @@
 package DataAccess;
 
+import Model.Event;
+import Model.Person;
+
+import java.util.ArrayList;
+
 public class PersonDAO {
+    /**
+     * Inserts a new Person to data
+     * @param person
+     */
+    public void insert(Person person){
+
+    }
+
+    /**
+     * Deletes a given Person from the data
+     * @param personID
+     */
+    public void delete(String personID){
+
+    }
+
+    /**
+     * Gets a Person from the data
+     * @param personID
+     * @return person
+     */
+    public Person get(String personID){
+
+        Person person = null;
+        return person;
+    }
+
+    /**
+     * Gets all the person related to the user
+     * @param user
+     * @return persons
+     */
+    public Person[] getAll(String user){
+        ArrayList<Person> persons = new ArrayList<>();
+        return null;
+    }
+
+    /**
+     * Deletes all the people from the table
+     */
+    public void clear(){
+
+    }
 }
--- src/main/java/DataAccess/UserDAO.java
+++ src/main/java/DataAccess/UserDAO.java
@@ -1,4 +1,40 @@
 package DataAccess;
 
+import Model.User;
+
 public class UserDAO {
+
+    /**
+     * Inserts a new user to data
+     * @param user
+     */
+    public void insert(User user){
+
+    }
+
+    /**
+     * Deletes a given user from the data
+     * @param username
+     */
+    public void delete(String username){
+
+    }
+
+    /**
+     * Gets a user from the data
+     * @param username
+     * @return user
+     */
+    public User get(String username){
+
+        User user = null;
+        return user;
+    }
+
+    /**
+     * Deletes all the usernames from the table
+     */
+    public void clear(){
+
+    }
 }
